1910a2e07cedb52a160a610ab22ee4888ed62a68
ntoscano <Nicolas.Toscano@gmail.com>
2015-08-29 12:20:17 -0700

    set up server.js, middleware.js, and user router

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,1 +1,17 @@
 // middleware.js
+var bodyParser = require('body-parser');
+var express = require('express');
+var userRoutes = require('../routes/users');
+var eventRoutes = require('../routes/events');
+var locationRoutes = require('../routes/locations');
+
+module.exports = function(app) {
+  app.use(bodyParser.json());
+
+  app.use(express.static(__dirname + '/../../client/build/'));
+
+  eventRoutes(app);
+  userRoutes(app);
+  locationRoutes(app);
+};
+
--- server/routes/users.js
+++ server/routes/users.js
@@ -1,1 +1,11 @@
 // users.js
+var userController = require('./userController');
+
+module.exports = function(app) {
+  app.post('/users/signup', userController.signup);
+  app.post('/user/login', userController.login);
+  app.get('/user/:id', userController.getUser);
+  app.post('/user/:id', userController.updateUser);
+  app.get('/users/:id/reviews', userController.getReview);
+  app.post('/users/:id/reviews', userController.postReview);
+};
