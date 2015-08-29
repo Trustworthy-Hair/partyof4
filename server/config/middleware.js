c1ee5923226aefa0c2260c45b4153a7d5f15fd1d
ntoscano <Nicolas.Toscano@gmail.com>
2015-08-29 14:48:22 -0700

    fixed event routes

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -1,9 +1,9 @@
 // middleware.js
 var bodyParser = require('body-parser');
 var express = require('express');
-var userRoutes = require('../routes/users.js');
-var eventRoutes = require('../routes/events.js');
-var locationRoutes = require('../routes/locations.js');
+var userRoutes = require('../routes/users');
+var eventRoutes = require('../routes/events');
+var locationRoutes = require('../routes/locations');
 
 module.exports = function(app) {
   app.use(bodyParser.json());
