2ccab3afa83c947e2e187e4d33a909c51cf1cfef
Brandon John Lewis <bjlewis88@gmail.com>
2015-08-29 15:05:14 -0700

    (feat) created sequelize users model

--- server/models/users.js
+++ server/models/users.js
@@ -1,1 +1,42 @@
 // users.js
+
+module.exports = function (sequelize, DataTypes) {
+  var User = sequelize.define({
+    username: {
+      type: DataTypes.STRING,
+      validate: {
+        notNull: true
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        notNull: true
+      }
+    }
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: true,
+        notNull: true
+      }
+    },
+    profileImageUrl: {
+      type: DataTypes.STRING,
+      validate: {
+        isUrl: true
+      }
+    },
+    interests: DataTypes.ARRAY,
+    description: DataTypes.TEXT,
+    status: {
+      type: DataTypes.STRING,
+      validate: {
+        notNull: true
+      }
+    },
+    connectedToFacebook: DataTypes.BOOLEAN
+  });
+
+  return User;
+};
