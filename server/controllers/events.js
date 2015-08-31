c225cb6460cbf6234713fa223f99e24c50f83c25
Brandon John Lewis <bjlewis88@gmail.com>
2015-08-31 16:44:31 -0700

    built createEvent controller

--- server/controllers/events.js
+++ server/controllers/events.js
@@ -1,13 +1,37 @@
 // events.js
-var Event = require('../models/events');
+var utils = require('../config/utils');
 
 module.exports = {
   getNearbyEvents: function(){
-    console.log(Event);
+    /* Select all events from events table where distance
+       between user and events < some number. */
   },
 
-  createEvent: function(){
+  createEvent: function(req, res){
+    var models = req.app.get('models');
+    var Event = models.Event;
 
+    /* The newEvent object contains a number of properties -- some obtained
+       from the body of the request and some defaults. This object is used
+       to build the Sequelize model that will be saved in the database. */
+    var newEvent = {};
+    newEvent.HostId = req.body.HostId;
+    newEvent.locationId = req.body.locationId;
+    newEvent.plannedTime = req.body.plannedTime;
+    newEvent.capacity = req.body.capacity;
+    newEvent.currentSize = 1;
+    newEvent.currentActivity = "Planning";
+    newEvent.completedStatus = false;
+
+    /* Builds a Sequelize model based on newEvent object and saves to the database. 
+       Sends a response if this is successful. Otherwise, logs an error. */
+    Event.sync().then(function () {
+      return Event.create(newEvent);
+    }).then (function (newEvent) {
+      utils.sendResponse(res, 201, newEvent);
+    }).catch(function (err) {
+      console.log('Error: ', err);
+    });
   },
 
   getEvent: function(){
