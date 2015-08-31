04eff295b958efdfab144b63682ef27b41a70ae9
Brandon John Lewis <bjlewis88@gmail.com>
2015-08-31 14:24:03 -0700

    modified open issues on userEvents model

--- server/models/userEvents.js
+++ server/models/userEvents.js
@@ -2,15 +2,7 @@
 
 /*
 Some Open Issues:
-  - Add some additional validation to these fields:
-    - None of these should be null, except maybe currentActivity
-    - Capacity should be greater than 0
-    - Current size should be >= 0 and <= capacity
-  - Add some default values
-    - completedStatus: false
-    - currentActivity: null?
-Note:
-  - This will have createdAt and updatedAt fields that we may need to use.
+  - Add additional validation, particularly for null values
 */
 
 module.exports = function (sequelize, DataTypes) {
