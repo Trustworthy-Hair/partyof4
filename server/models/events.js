0988202894c8eaa56cc5c4ffb64f17c62eafd1ff
Brandon John Lewis <bjlewis88@gmail.com>
2015-08-31 11:46:41 -0700

    created events model

--- server/models/events.js
+++ server/models/events.js
@@ -1,1 +1,27 @@
 // events.js
+
+/*
+Some Open Issues:
+  - Add some additional validation to these fields:
+    - None of these should be null, except maybe currentActivity
+    - Capacity should be greater than 0
+    - Current size should be >= 0 and <= capacity
+  - Add some default values
+    - completedStatus: false
+    - currentActivity: null?
+Note:
+  - This will have createdAt and updatedAt fields that we may need to use.
+*/
+
+module.exports = function (sequelize, DataTypes) {
+  var Event = sequelize.define('Event', {
+    locationId: DataTypes.INTEGER,
+    plannedTime: DataTypes.DATE,
+    capacity: DataTypes.INTEGER,
+    currentSize: DataTypes.INTEGER,
+    currentActivity: DataTypes.STRING,
+    completedStatus: DataTypes.BOOLEAN
+  });
+
+  return Event;
+};
