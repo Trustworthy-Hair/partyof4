8ad43a762838cb8ec110936efc8c39a3d4de0f92
Brandon John Lewis <bjlewis88@gmail.com>
2015-09-02 10:25:11 -0700

    refactored database initialization code

--- server/config/db.js
+++ server/config/db.js
@@ -28,12 +28,7 @@ module.exports.Event = Event;
 module.exports.UserEvent = UserEvent;
 module.exports.Review = Review;
 
+
 // RUN THE FOLLOWING CODE TO MAKE SURE TABLES ARE ADDED CORRECTLY
 
-// User.sync().then(function () {
-//   return Event.sync();
-// }).then(function () {
-//   return UserEvent.sync();
-// }).then(function () {
-//   return Review.sync();
-// });
+// sequelize.sync();
