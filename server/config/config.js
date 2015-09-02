0eed56789b555d6e32cf2ac8679d5873c7bc255c
Brandon John Lewis <bjlewis88@gmail.com>
2015-09-02 10:42:32 -0700

    refactored database config to create connection in production environment

--- server/config/config.js
+++ server/config/config.js
@@ -1,15 +1,7 @@
 // config.js
 
 if (process.env.NODE_ENV === 'production') {
-  module.exports.database = 'INSERT DATABASE';
-  module.exports.username = 'INSERT USERNAME';
-  module.exports.password = 'INSERT PASSWORD';
-  module.exports.host = 'INSERT HOST';
+  module.exports.databaseUrl = process.env.DATABASE_URL;
 } else {
-  module.exports.database = 'partyof4';
-  module.exports.username = null;
-  module.exports.password = null;
-  module.exports.host = '127.0.0.1';
+  module.exports.databaseUrl = 'postgres://127.0.0.1:5432/partyof4';
 }
-
-module.exports.dialect = 'postgres';
