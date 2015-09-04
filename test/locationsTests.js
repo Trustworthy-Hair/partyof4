ba98c85e00b4156bc50e25a0e22de30c1da9d2d9
Shelley Wang <shelleyw13@gmail.com>
2015-09-03 20:37:19 -0700

    Add tests for /locations get request

---
+++ test/locationsTests.js
@@ -0,0 +1,47 @@
+process.env.NODE_ENV = 'test';
+
+var supertest = require('supertest');
+var server = require('../server/server');
+var assert = require('assert');
+var expect = require('expect.js');
+
+var request = supertest(server);
+
+var testData = {};
+
+describe('routes', function() {
+
+  before(function(done){
+    server.get('models').sequelize.sync({ force: true}).then(function () {
+      done();
+    });
+  });
+
+  beforeEach(function () {
+
+  });
+
+  describe('GET /locations', function() {
+    it('should return items from /locations when given a proper latitude and longitude', function(done) {
+      request
+        .get('/locations?latitude=37.7837209&longitude=-122.4090445')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
+        .expect(200, done);
+    });
+
+    it ('should not return a 200 status code when latitude and longitude are missing or invalid', function(done) {
+      request
+        .get('/locations')
+        .set('Accept', 'application/json')
+        .expect(400);
+
+            request
+        .get('/locations?latitude=1000&longitude=-1000')
+        .set('Accept', 'application/json')
+        .expect(400, done);
+    })
+  });
+
+
+});
