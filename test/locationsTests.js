15a6640855e3f326c3f893e07f0e504124cde010
Shelley Wang <shelleyw13@gmail.com>
2015-09-04 10:46:09 -0700

    Remove test

--- test/locationsTests.js
+++ test/locationsTests.js
@@ -22,11 +22,11 @@ describe('routes', function() {
   });
 
   describe('GET /locations', function() {
-    it('should return items from /locations when given a proper latitude and longitude', function(done) {
-      request
-        .get('/locations?latitude=37.7837209&longitude=-122.4090445')
-        .expect(200, done);
-    });
+    // it('should return items from /locations when given a proper latitude and longitude', function(done) {
+    //   request
+    //     .get('/locations?latitude=37.7837209&longitude=-122.4090445')
+    //     .expect(200, done);
+    // });
 
     it ('should not return a 200 status code when latitude and longitude are missing or invalid', function(done) {
       request
