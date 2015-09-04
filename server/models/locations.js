b78550e29096fc1152704adf521bfca49b6f0da5
Shelley Wang <shelleyw13@gmail.com>
2015-09-04 11:40:48 -0700

    Add new locations table for storing venue data

---
+++ server/models/locations.js
@@ -0,0 +1,19 @@
+// locations.js
+// caches location information from foursquare 
+
+// TO-DO: Foursquare ToS says data can be stored for 30 days before it must be updated. 
+// Any data that is older than 30 days should be removed. 
+
+module.exports = function (sequelize, DataTypes) {
+  var Location = sequelize.define('Location', {
+    locationId: DataTypes.INTEGER,
+    fourSquareId: DataTypes.STRING(24),
+    name: DataTypes.STRING,
+    address: DataTypes.JSON,
+    longitude: DataTypes.DOUBLE,
+    latitude: DataTypes.DOUBLE,
+    tags: DataTypes.ARRAY(DataTypes.TEXT),
+  });
+
+  return Location;
+};
