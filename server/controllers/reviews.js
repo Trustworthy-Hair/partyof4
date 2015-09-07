a719390bff8c7c404d41b583da1e61b8fd898df7
Shelley Wang <shelleyw13@gmail.com>
2015-09-07 10:55:31 -0700

    Write get handler for reviews

--- server/controllers/reviews.js
+++ server/controllers/reviews.js
@@ -2,8 +2,39 @@ var utils = require('../config/utils');
 
 module.exports = {
 
-  getReviews: function(){
-    console.log(utils);
+  getReviews: function(req,res){
+    var userId = req.params.id;
+    var models = req.app.get('models');
+    var Review = models.Review;
+
+    Review.sync().then(function() {
+      return Review.findAll({
+        where: {subjectId: userId}
+      });
+    })
+    .then(function(results) {
+      var totalStar = results.reduce(function(previous, current) {
+        return previous + current.starRating;
+      }, 0);
+      var averageStar = totalStar / results.length;
+
+      var reviews = results.map(function(data) {
+        return {
+          authorId: data.authorId,
+          eventId: data.eventId,
+          starRating: data.starRating,
+          text: data.text, 
+          createdAt: data.createdAt
+        };
+      });
+
+      var reviewData = {
+        averageRating: averageStar,
+        reviews: reviews
+      };
+
+      utils.sendResponse(res,200,reviewData);
+    });
   },
 
   postReview: function(){
